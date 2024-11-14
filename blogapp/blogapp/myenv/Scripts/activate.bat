297a8a766887dc93adfae9d7dfdc2051b612ce40
Ataberk Kaya <ataberk.kaya@icloud.com>
2024-11-14 16:23:30 +0300

    create new env and requirements update

--- blogapp/blogapp/myenv/Scripts/activate.bat
+++ blogapp/blogapp/myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\atabe\Desktop\Django_Task\blogapp\myenv
+set VIRTUAL_ENV=C:\Users\atabe\Desktop\Django_Task_Kopya\blogapp\blogapp\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
